6430f9ff4b35bed20537f77f3cc68358adec4ec0
devc1234 <namdevchavan713@gmail.com>
2024-06-13 04:56:53 +0530

    my forth commit

--- src/main/java/com/dev/CurdApp/controller/EmployeController.java
+++ src/main/java/com/dev/CurdApp/controller/EmployeController.java
@@ -3,9 +3,7 @@ package com.dev.CurdApp.controller;
 import com.dev.CurdApp.model.Employe;
 import com.dev.CurdApp.repo.EmployeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,11 @@ public class EmployeController {
     @GetMapping
     public List<Employe> getAllEmployees(){
         return  employeRepo.findAll();
+    }
+
+
+    @PostMapping
+    public Employe createEmploye(@RequestBody Employe employe){
+        return employeRepo.save(employe);
     }
 }
