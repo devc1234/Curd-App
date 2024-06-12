06ee8e6ed8c1634a5cdfd35c3bcfd102ea4fd69a
devc1234 <namdevchavan713@gmail.com>
2024-06-13 04:22:12 +0530

    my third commit

--- src/main/java/com/dev/CurdApp/CurdAppApplication.java
+++ src/main/java/com/dev/CurdApp/CurdAppApplication.java
@@ -1,13 +1,36 @@
 package com.dev.CurdApp;
 
+import com.dev.CurdApp.model.Employe;
+import com.dev.CurdApp.repo.EmployeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurdAppApplication {
+public class CurdAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurdAppApplication.class, args);
 	}
 
+	@Autowired
+	EmployeRepo employeRepo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employe employe =new Employe();
+		employe.setId(12);
+		employe.setFirstname("dev");
+		employe.setLastName("chavan");
+		employe.setEmailId("namdev@gmail.com");
+		employeRepo.save(employe);
+
+		Employe employe1 = new Employe();
+		employe1.setId(12);
+		employe1.setFirstname("rajjev");
+		employe1.setLastName("chavan");
+		employe1.setEmailId("rajeevgmail.com");
+		employeRepo.save(employe1);
+	}
 }
--- src/main/java/com/dev/CurdApp/controller/EmployeController.java
+++ src/main/java/com/dev/CurdApp/controller/EmployeController.java
@@ -1,13 +1,23 @@
 package com.dev.CurdApp.controller;
 
+import com.dev.CurdApp.model.Employe;
 import com.dev.CurdApp.repo.EmployeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/employes")
 public class EmployeController {
 
     @Autowired
     private EmployeRepo employeRepo;
 
+    @GetMapping
+    public List<Employe> getAllEmployees(){
+        return  employeRepo.findAll();
+    }
 }
